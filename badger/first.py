57433bf5065e13284860fa6cb73f6b44bf2b3422
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2022-04-14 21:17:17 +0200

    More examples

--- badger/first.py
+++ badger/first.py
@@ -1,6 +1,9 @@
 import badger2040
-badger = badger2040.Badger2040()
 
+badger = badger2040.Badger2040()
 badger.pen(0)
-badger.text("Hello Kitty", 20, 20)
+badger.text("meow", 20, 20)
 badger.update()
+
+badger.pen(15)
+badger.clear()
