b7aa930da4506e78c5c75600922565f777879687
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2022-04-18 09:53:10 +0200

    Try behave - Python's Cucumber tutorial

---
+++ behave/features/steps/tutorial.py
@@ -0,0 +1,15 @@
+# Tutorial on Behave from: https://behave.readthedocs.io/en/stable/tutorial.html
+
+from behave import *
+
+@given('we have behave installed')
+def step_impl(context):
+    pass
+
+@when('we implement a test')
+def step_impl(context):
+    assert True is not False
+
+@then('behave will test it for us!')
+def step_impl(context):
+    assert context.failed is False
